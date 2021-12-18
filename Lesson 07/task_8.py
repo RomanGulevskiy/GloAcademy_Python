e2a90615f177aa3a4c34c9db19b1e7b7d5ae3b96
Roman Gulevskiy <roman.gulevskiy@gmail.com>
2021-12-18 23:11:24 +0500

    Update task_8.py

--- Lesson 07/task_8.py
+++ Lesson 07/task_8.py
@@ -1,14 +1,12 @@
 numbers = int(input('Введите число равное 2 или больше: '))
 min = 0
 max = 0
-counter = 0
 
-for i in range(1, numbers + 1):
+for i in range(numbers + 1):
     user_number = int(input('Введите число: '))
-    if counter == 0:
+    if i == 0:
         min = user_number
         max = user_number
-        counter = 1
     if user_number > max:
         max = user_number
     elif user_number < min:
